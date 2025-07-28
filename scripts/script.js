8757f74143e973bdf47576d61e3b6d50a598f72a
JACE062 <123290590+JACE062@users.noreply.github.com>
2025-07-28 00:41:26 -0600

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const themeToggle = document.getElementById('theme-toggle');
+    const body = document.body;
+
+
+    const savedTheme = localStorage.getItem('theme');
+    if(savedTheme === 'dark') {
+        body.classList.add('dark-theme');
+        themeToggle.textContent = '☀️ Light Mode'
+    }
+
+    themeToggle.addEventListener('click', function() {
+        body.classList.toggle('dark-theme');
+
+        const isDark = body.classList.contains('dark-theme');
+
+        themeToggle.textContent = isDark ? '☀️ Light Mode' : '🌙 Dark Mode'
+
+        localStorage.setItem('theme', isDark ? 'dark' : 'light');
+
+    });
+
+
+
+});
